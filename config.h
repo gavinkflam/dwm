d459bee7c80d29d1bc2c57995c63f841f798ddf1
Gavin Lam <me@gavin.hk>
2018-08-21 00:07:01 +0800

    Removed dmenu font and color scheme options

--- config.h
+++ config.h
@@ -58,8 +58,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = {
-  "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb",
-  normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL
+  "dmenu_run", "-m", dmenumon, NULL
 };
 static const char *termcmd[]  = { "st", NULL };
 
